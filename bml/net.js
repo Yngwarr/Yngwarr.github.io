2d24a2088309c9c3e0bc0055c2702c58d8d68145
Yngwarr <k.yngvarr@gmail.com>
2019-09-27 12:42:17 +0700

    pass all the questions

--- bml/net.js
+++ bml/net.js
@@ -1,9 +1,27 @@
 let answers = {};
 
-{
-    let tmp;
-    tmp = clear_text('Укажите имя команды, с помощью которой можно определить количество маршрутизаторов на пути от Вашего компьютера к заданному Web-сайту ?');
-    answers[tmp] = 'traceroute';
+db_add(answers,
+    'Укажите имя команды, с помощью которой можно определить количество маршрутизаторов на пути от Вашего компьютера к заданному Web-сайту ?',
+    'traceroute');
+
+db_add(answers,
+    'Какие действия предусмотрены протоколом ICMP при потере ICMP - пакета ?',
+    'формирование нового ICMP - пакета, содержащего сообщение о потере;');
+
+db_add(answers,
+    'Какие поля входят в структуру ICMP-сообщения ?',
+    ['длина сообщения', 'тип сообщения', 'контрольная сумма']);
+
+db_add(answers,
+    'Верно ли, что обработка ICMP-сообщения не входит в обязанности протоколов IP или ICMP ?',
+    false);
+
+function db_add(db, q, a) {
+    let ans;
+    if (a === true || a === false) ans = a;
+    else if (Array.isArray(a)) ans = a.map(x => clear_text(x));
+    else ans = clear_text(a);
+    return db[clear_text(q)] = ans;
 }
 
 function clear_text(text) {
@@ -24,17 +42,40 @@ function main() {
 
     let t = document.querySelector(".qtext");
     let q = clear_text(t.innerText);
-    //let as = [];
+    let opts;
     
-    if (q_type === 'text') {
-        document.querySelector(`.answer input[type='text']`).value = answers[q];
+    switch (q_type) {
+        case 'text':
+            document.querySelector(`.answer input[type='text']`).value = answers[q];
+            break;
+        case 'radio':
+            opts = document.querySelectorAll(`.answer input[type='radio']`);
+            opts.forEach(el => {
+                let cans = clear_text(el.labels[0].innerText);
+                if (answers[q] === true || answers[q] === false) {
+                    if ((answers[q] && cans === 'верно') || (!answers[q] && cans === 'неверно')) {
+                        el.click();
+                    }
+                } else {
+                    if (cans.includes(answers[q])) {
+                        el.click();
+                    }
+                }
+            });
+            break;
+        case 'checkbox':
+            opts = document.querySelectorAll(`.answer input[type='checkbox']`);
+            opts.forEach(el => {
+                let cans = clear_text(el.labels[0].innerText);
+                for (let i = 0; i < answers[q].length; ++i) {
+                    if (cans.includes(answers[q][i])) {
+                        el.click();
+                        break;
+                    }
+                }
+            });
+            break;
     }
-
-    //if (q_type !== 'text') {
-        //let a = document.querySelector(".answer");
-        //for (let i = 0; i < as.childElementCount; ++i) { as.push(a.children[i].innerText); }
-        //as = as.map(clear_text);
-    //}
 }
 
 console.log('injecting...');
